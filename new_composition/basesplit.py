4f662809d39cc077da445f2ea6de85acc389f326
Klaryce <936032450@qq.com>
2021-05-11 18:10:23 +0800

    structure changes

--- new_composition/basesplit.py
+++
@@ -1,9 +0,0 @@
-from helpfuncs import bitdecoding
-from glob import globs
-
-N = globs["size"]
-
-# initialize and fill list for set based on base relations
-bsplit = [(len(bitdecoding(i+1)),bitdecoding(i+1)) for i in range((2**N)-1)]
-
-
